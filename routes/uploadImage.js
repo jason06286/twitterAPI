9f24b12debf378ed36570818b2fe83ef8e64a173
jason06286 <dj4871114@gmail.com>
2022-07-12 01:29:34 +0800

    feat: imgupload

--- routes/uploadImage.js
+++ routes/uploadImage.js
@@ -9,6 +9,5 @@ const uploadImageControllers = require('../controllers/uploadImage');
  * POST /api/uploadImage{ data?: any }
  */
 router.post('/', isAuth, checkUpload, uploadImageControllers.uploadImage);
-router.post('/base64', isAuth, checkUpload, uploadImageControllers.uploadBase64Image);
 
 module.exports = router;
